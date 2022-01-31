fa23063f4daa9816ea0c45673cf0af705c93e699
RandallBG <Randall.bg.gosnell@gmail.com>
2022-01-31 14:31:40 -0500

    Added nav

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+let Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact us</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
