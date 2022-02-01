2cbe69483cd11517a21c343256c13670adc1e1cd
RandallBG <Randall.bg.gosnell@gmail.com>
2022-02-01 14:39:13 -0500

    divs out of place fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,41 +1,37 @@
-
-
 const Projects = () => {
   return (
     <div>
-      <h1>What we can do</h1>
       <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
+        <h1>What we can do</h1>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
       </div>
       <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
+        <h1>Together</h1>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
+        <div>
+          <img src={""} alt="projects" />
+          <p></p>
+        </div>
       </div>
-      <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
     </div>
-    <div>
-      <h1>Together</h1>
-      <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
-      </div>
-      <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
-      </div>
-      <div>
-      <img src={require('#')} alt="projects" />
-      <p></p>
-      </div>
-    </div> 
-
-
-    </div> 
-    
-    
   );
 };
 
