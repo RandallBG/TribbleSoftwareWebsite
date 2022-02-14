fd833a36a67d8948ff0ae662500371aaf8f47ded
RandallBG <Randall.bg.gosnell@gmail.com>
2022-02-14 16:25:13 -0500

    updated web copy

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,27 +34,13 @@ const About = () => {
         <div className="textSection">
           <h1 className="aboutTitle">How we can help you</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Fermentum dui faucibus in ornare. Tincidunt eget nullam non nisi.
-            Ornare arcu odio ut sem nulla pharetra diam. Accumsan lacus vel
-            facilisis volutpat est velit egestas dui id. Convallis aenean et
-            tortor at risus viverra. Volutpat commodo sed egestas egestas
-            fringilla. Tincidunt arcu non sodales neque. Tristique nulla aliquet
-            enim tortor at auctor urna. Sem viverra aliquet eget sit amet tellus
-            cras adipiscing. Tortor id aliquet lectus proin nibh nisl
-            condimentum. Facilisi cras fermentum odio eu. Eget gravida cum
-            sociis natoque penatibus et magnis. Facilisis leo vel fringilla est
-            ullamcorper eget nulla facilisi etiam. A diam maecenas sed enim ut.
-            Non pulvinar neque laoreet suspendisse interdum consectetur.
-            Condimentum id venenatis a condimentum vitae sapien pellentesque
-            habitant. Non diam phasellus vestibulum lorem sed risus ultricies
-            tristique. Eu sem integer vitae justo eget magna fermentum iaculis
-            eu. Suscipit tellus mauris a diam maecenas sed enim ut sem. Neque
-            volutpat ac tincidunt vitae semper quis lectus nulla. Pharetra vel
-            turpis nunc eget. Maecenas pharetra convallis posuere morbi leo. Ac
-            feugiat sed lectus vestibulum mattis ullamcorper velit sed
-            ullamcorper.
+            Developing an online presence for your business has never been more
+            important than now. Here at Tribble, we help create a beautiful
+            website, meant to reach new customers and keep them coming back. We
+            lay our sites out in a way that ensures the most engagement
+            possible. Be that converting a sale, signing them up for a
+            newsletter, or just giving them the information they are looking for
+            in the easiest to find locations.
           </p>
         </div>
       </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,16 +11,19 @@ const Home = () => {
         <div className="columnBlurb">
           <h2>What we do</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam
-            pellentesque nec nam aliquam. Pharetra vel turpis nunc eget lorem
-            dolor. Imperdiet proin fermentum leo vel orci porta non. Euismod
-            elementum nisi quis eleifend quam adipiscing vitae proin. Sed nisi
-            lacus sed viverra tellus in hac habitasse platea. Interdum velit
-            laoreet id donec ultrices tincidunt. Morbi enim nunc faucibus a
-            pellentesque sit amet porttitor. Sit amet est placerat in egestas
-            erat. Sodales ut eu sem integer vitae. Nisl pretium fusce id velit.
-            Orci a scelerisque purus semper eget duis.
+            Here at Tribble software we strive to create stunning websites
+            tailored to your needs as a customer. We understand that every
+            business problem requires a hand crafted solution. This is why we
+            spend our time up front understanding you and your business needs.
+            Once we understand how our clients business works, we set off to
+            create something you can not only be proud of, but feel good showing
+            people. Keep in mind visuals are just the beginning. We take our
+            time to make sure everything on the page is laid out in a manner
+            that brings your customers to the correct information. We do this to
+            make sure you get the best engagements or sales possible. As a small
+            team we take pride in making sure you know you’re not just another
+            cog in the wheel, but one of our clients. To us, that means
+            something.
           </p>
         </div>
 
@@ -52,16 +55,13 @@ const Home = () => {
         <div className="columnBlurb">
           <h2>Our projects</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam
-            pellentesque nec nam aliquam. Pharetra vel turpis nunc eget lorem
-            dolor. Imperdiet proin fermentum leo vel orci porta non. Euismod
-            elementum nisi quis eleifend quam adipiscing vitae proin. Sed nisi
-            lacus sed viverra tellus in hac habitasse platea. Interdum velit
-            laoreet id donec ultrices tincidunt. Morbi enim nunc faucibus a
-            pellentesque sit amet porttitor. Sit amet est placerat in egestas
-            erat. Sodales ut eu sem integer vitae. Nisl pretium fusce id velit.
-            Orci a scelerisque purus semper eget duis.
+            We’ve had the pleasure of working with some great people over the
+            course of our time as a business. Creating a business is tough work
+            and we like to think we can help make the web aspect as smooth as
+            possible so you can focus your efforts at more important things. If
+            there’s one thing that makes us proud, its seeing our clients go on
+            to do great things and know we helped in that journey, even if only
+            a little.
           </p>
         </div>
 
