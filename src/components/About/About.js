b7cfaee0a714794f5c70f7e558efb059518f2fdf
xLaoztu <Derek_Gembus@yahoo.com>
2022-02-14 16:32:58 -0500

    updated who we are

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,27 +7,7 @@ const About = () => {
         <div className="textSection">
           <h1 className="aboutTitle">Who we are</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Fermentum dui faucibus in ornare. Tincidunt eget nullam non nisi.
-            Ornare arcu odio ut sem nulla pharetra diam. Accumsan lacus vel
-            facilisis volutpat est velit egestas dui id. Convallis aenean et
-            tortor at risus viverra. Volutpat commodo sed egestas egestas
-            fringilla. Tincidunt arcu non sodales neque. Tristique nulla aliquet
-            enim tortor at auctor urna. Sem viverra aliquet eget sit amet tellus
-            cras adipiscing. Tortor id aliquet lectus proin nibh nisl
-            condimentum. Facilisi cras fermentum odio eu. Eget gravida cum
-            sociis natoque penatibus et magnis. Facilisis leo vel fringilla est
-            ullamcorper eget nulla facilisi etiam. A diam maecenas sed enim ut.
-            Non pulvinar neque laoreet suspendisse interdum consectetur.
-            Condimentum id venenatis a condimentum vitae sapien pellentesque
-            habitant. Non diam phasellus vestibulum lorem sed risus ultricies
-            tristique. Eu sem integer vitae justo eget magna fermentum iaculis
-            eu. Suscipit tellus mauris a diam maecenas sed enim ut sem. Neque
-            volutpat ac tincidunt vitae semper quis lectus nulla. Pharetra vel
-            turpis nunc eget. Maecenas pharetra convallis posuere morbi leo. Ac
-            feugiat sed lectus vestibulum mattis ullamcorper velit sed
-            ullamcorper.
+            We're Tribble Software and we are a small team of web developers and designers who are passionate about creating beautiful and functional websites. We strive to provide our clients with the best possible experience and  tailor you website to your needs. We are recent graduates of the Tri-C Software Developers Academy and have been operating in the Cleveland, Ohio area for the past 6 months. We are a new company and are looking for new opportunities to grow and make our clients' lives easier.
           </p>
         </div>
 
