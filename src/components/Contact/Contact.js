e4330d589b60a43c562dabfd7b84d5e068bf3264
RandallBG <Randall.bg.gosnell@gmail.com>
2022-02-16 20:27:22 -0500

    added modal to pop up after contact form filled in
    Also Changed the readme text to future optimizations and stuff needed to be done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,47 @@
 import "./Contact.css";
+import Modal from "../modules/modal/Modal";
 import customerService from "../../images/CustomerServiceSmall.jpg";
+import { useEffect } from "react";
 
 const Contact = () => {
+  useEffect(() => {
+    const modal = document.getElementById("contactModal");
+    const sendMailForm = document.getElementById("contactSubmitButton");
+
+    async function sendForm() {
+      let xhr = new XMLHttpRequest();
+
+      xhr.open("POST", "./react-backend/mailInstructions.php", true);
+      let data = new FormData(document.getElementById("contactForm"));
+
+      xhr.send(data);
+
+      xhr.onload = function () {
+        if (xhr.status != 200) {
+          document.getElementById("contactSubmitButton").innerHTML = "Error";
+        } else {
+          modal.style.display = "flex";
+          document.getElementById("contactSubmitButton").disabled = true;
+          document.getElementById("contactSubmitButton").innerHTML = "Sent!";
+        }
+      };
+    }
+
+    sendMailForm.addEventListener("click", (e) => {
+      e.preventDefault();
+      sendForm();
+    });
+  }, []);
+
   return (
     <div className="contactWrapper">
       <div>
         <div className="contactContainer">
-          <form id="contactForm" action="./react-backend/mailInstructions.php">
+          <form
+            id="contactForm"
+            action="./react-backend/mailInstructions.php"
+            method="POST"
+          >
             <div className="form-wrapper">
               <div className="form-group">
                 <label>Name:</label>
@@ -61,9 +96,7 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      {/* <div className="contactWrapper">
-        <Modal />
-      </div> */}
+      <Modal />
     </div>
   );
 };
--- src/components/modules/modal/Modal.js
+++ src/components/modules/modal/Modal.js
@@ -1,24 +1,18 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Modal.css";
-import Page from "./ModalPage";
 
 const Modal = () => {
-  useEffect(() => {
-    const toggleModal = () => {
-      document.getElementById("contactModal").classList.toggle("modalActive");
-    };
-
-    document.getElementById("contactModal").addEventListener("click", (e) => {
-      console.log("clicked");
-      toggleModal();
-    });
-  });
   return (
-    <div id="contactModal" className="modal modalActive">
-      <Page
-        title="Whats your name?"
-        inputs={["First Name", "Last Name"]}
-      ></Page>
+    <div id="contactModal" className="modalWrapper">
+      <div className="modalContent">
+        <h1>Thank You!</h1>
+
+        <h2>We will be in touch with you shortly.</h2>
+      </div>
+      <Link to="/" id="closeModal" className="closeModal">
+        Close
+      </Link>
     </div>
   );
 };
